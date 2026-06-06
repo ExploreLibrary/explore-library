719a3a1bfd61a6f8149238d25f3f0c454f19aa5e
David Masegosa <davidmasegosa@gmail.com>
2026-06-06 14:49:54 +0200

    feat: Create basic Navbar #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Navbar from "./components/ui/navbar.jsx";
+import Gallery from "./components/ui/gallery.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <div
+        style={{
+          maxWidth: 1400,
+          marginInline: "auto",
+        }}
+      >
+        <Gallery />
+      </div>
+    </>
+  );
 }
 
 export default App;
