041d054cd4096e20f0192b1ef71332db50dbd325
Juan <juancot.pe@gmail.com>
2026-06-11 19:58:56 +0200

    feat: add loader to trendingbooks

--- src/components/ui/gallery.jsx
+++ src/components/ui/gallery.jsx
@@ -1,20 +1,21 @@
 import Book from "./book.jsx";
 import { useState, useEffect } from "react";
 import * as BooksService from "../../services/trending-books-services.js";
+import Loader from "./loader.jsx";
 
 function Gallery() {
   let [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchBooks() {
       try {
         const booksData = await BooksService.listTrendingBooks();
-
-
         setBooks(booksData);
-        console.log(booksData);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     }
     fetchBooks();
@@ -22,33 +23,38 @@ function Gallery() {
 
   return (
     <>
-      <h2 style={{ paddingBlock: 20 }}>Libros en tendencia</h2>
-      <ul
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: 10,
-          listStyleType: "none",
-          padding: 0,
-        }}
-      >
-        {books &&
-          books.map((book) => {
-            const description = book.first_sentence?.[0];
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h2 style={{ paddingBlock: 20 }}>Libros en tendencia</h2>
+          <ul
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: 10,
+              listStyleType: "none",
+              padding: 0,
+            }}
+          >
+            {books &&
+              books.map((book) => {
+                const description = book.first_sentence?.[0];
+                const cover = book.cover_i;
 
-            const cover = book.cover_i;
-
-            return (
-              <li key={book.key} style={{ flex: "0 0 calc(16.66% - 9px)" }}>
-                <Book
-                  title={book.title}
-                  description={description}
-                  cover={cover}
-                />
-              </li>
-            );
-          })}
-      </ul>
+                return (
+                  <li key={book.key} style={{ flex: "0 0 calc(16.66% - 9px)" }}>
+                    <Book
+                      title={book.title}
+                      description={description}
+                      cover={cover}
+                    />
+                  </li>
+                );
+              })}
+          </ul>
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/ui/loader.jsx
@@ -0,0 +1,11 @@
+import LoaderImage from "../../assets/loading.gif"
+
+function Loader() {
+  return (
+    <div>
+      <img src={LoaderImage} alt="Loading" />
+    </div>
+  )
+}
+
+export default Loader;
